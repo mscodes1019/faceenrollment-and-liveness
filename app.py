b0db03a0f15ffc7ec648309f9b9f49cc557a29a9
Monsurat Ayinde <mayinde08@gmail.com>
2026-04-15 17:58:16 +0100

    Initial push to new repo

---
+++ app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.title("FaceAuth Demo Video")
+
+video_path = r"C:\Users\MonsuratAyinde\Desktop\faceidentificationandenrollment\facelivenessandverification.mp4"
+
+st.video(video_path)
